5762da0aa9e4478ca0a012dac6a143c5db01bbc7
Frankie Robertson <frankie@robertson.name>
2024-08-08 11:56:31 +0300

    Add debug server script

---
+++ debug.jl
@@ -0,0 +1,28 @@
+using Revise
+using Oxygen
+using CATServe: ws_handler, update_templates, CATServe
+
+function ReviseHandler(handle)
+    req -> begin
+        if length(Revise.revision_queue) > 0
+            @time "Sync revision" Revise.revise()
+        end
+        invokelatest(handle, req)
+    end
+end
+
+@time "Initial revision" Revise.revise()
+CATServe.serve(; host="127.0.0.1", port=8001, handler=ws_handler, middleware=[ReviseHandler], async=true)
+key = Revise.add_callback(["templates"]) do
+    @info "Starting template update"
+    @time "Template update" invokelatest(update_templates)
+end
+try
+    while true
+        wait(Revise.revision_event)
+        @info "Starting async revision"
+        @time "Async revision" Revise.revise()
+    end
+finally
+    Revise.remove_callback(key)
+end
