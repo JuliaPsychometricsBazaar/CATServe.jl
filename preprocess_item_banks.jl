585528a97134b024f668ba3a6c3543839e9835bf
Frankie Robertson <frankie@robertson.name>
2024-08-08 11:53:37 +0300

    Fix preprocess_item_banks

--- preprocess_item_banks.jl
+++ preprocess_item_banks.jl
@@ -1,24 +1,23 @@
 using ItemResponseDatasets: VocabIQ
-using RIrtWrappers.Mirt: fit_mirt
-using RIrtWrappers.KernSmoothIRT
+using RIrtWrappers.Mirt: Mirt
+using RIrtWrappers.KernSmoothIRT: KernSmoothIRT
 using Serialization
 using Base.Filesystem
 
 
 function vocabiq_4pl_1d()
     marked_df = VocabIQ.get_marked_df_cached()
-    fit_4pl(marked_df; TOL=1e-2)[1], VocabIQ.questions
+    Mirt.fit_4pl(marked_df; TOL=1e-2)[1], VocabIQ.questions
 end
 
 function vocabiq_monopoly()
     marked_df = VocabIQ.get_marked_df_cached()
-    params = fit_mirt(marked_df; itemtype="monopoly", var"monopoly.k"=1)
-    fit_4pl(marked_df; TOL=1e-2)[1], VocabIQ.questions
+    Mirt.fit_monopoly(marked_df; TOL=1e-2)[1], VocabIQ.questions
 end
 
 function vocabiq_ksirt()
     marked_df = VocabIQ.get_marked_df_cached()
-    fit_ks_dichotomous(marked_df)[1], VocabIQ.questions
+    KernSmoothIRT.fit_ks_dichotomous(marked_df)[1], VocabIQ.questions
 end
 
 function main(outdir)
